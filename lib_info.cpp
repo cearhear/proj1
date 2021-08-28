c3b120b91fbec106db8b2aa5b4dc49677c7a24e7
Stephanie Hill <shill77@hydra0.eecs.utk.edu>
2021-08-28 16:53:51 -0400

    lib_info.cpp

--- lib_info.cpp
+++ lib_info.cpp
@@ -1,11 +1,15 @@
-/* COSC302 Project1
+/* 
+ * COSC302 Project1
  * Carter Earheart-Brown
  * Stephanie Hill
  * Purpose: Import music information from file and organize it by 
  *			artist and album.
  */
 #include<iostream>
+#include<fstream>
+#include<sstream>
 #include<string>
+#include<algorithm>
 #include<map>
 
 using namespace std;
@@ -13,7 +17,7 @@ using namespace std;
 //Borrowed from assignment instructions
 struct Song {
     string title;
-    string time;  // could also be a string
+    int time;  // could also be a string
     int track;
 };
 
@@ -31,20 +35,48 @@ struct Artist {
     int nsongs;
 };
 
-/* Takes in the imported time as a string,
- * converts it to an integer
+/*
+ * Takes in the imported time as a string,
+ * seperates it, converts it to an integer
  * and exports the total time in seconds. 
  */
-int timeConvertMtoS(string){
+int timeConvertMtoS(string time){
+	int totsec, minutes, seconds;
 	string min, sec;
-	int minutes, seconds, totsec;
-	getline(
-	minutes = stoi(min);
-	minutes = stoi(sec);
+
+	min=time.substr(0, time.find(":"));
+	sec=time.substr(time.find(":") + 1);
+	
+	minutes=stoi(min);
+	seconds=stoi(sec);
+	
 	return totsec = (minutes * 60) + seconds;
 }
 
-/* Takes in a time integer as seconds,
+/*
+ * Reads in file and converts all underscores to spaces 
+ * Source: https://stackoverflow.com/questions/7868936/read-file-line-by-line-using-ifstream-in-c
+ * https://stackoverflow.com/questions/2896600/how-to-replace-all-occurrences-of-a-character-in-string
+*/
+void readFile(string file){
+
+	fstream infile(file);
+	string line, title, time, artist, album, genre;
+	int track; 
+	
+	while (getline(infile, line)){
+		istringstream iss(line); 
+		//replace(line.begin(), line.end(), '_', ' '); //this line will remove underscores (I'm not sure where to place it)
+		iss >> title >> time >> artist >> album >> genre >> track; //reads in values and assigns to variables
+				
+		//cout<< title << " " << timeConvertMtoS(time) << endl; //testing
+	
+	}
+	infile.close();
+}
+
+/* 
+ * Takes in a time integer as seconds,
  * calculates the minutes and remaining seconds,
  * and exports it in the form MM:SS
  */
@@ -55,21 +87,12 @@ string timeConvertStoM(int)
 	minutes = seconds / 60;
 	return minutes + ": " +rem;
 }
-//function to print the output
-void print(){
-	// still need to write this
-}
 
-int main(){
+int main(int argc, char *argv[]){
 	string song, time, artist, album, genre;
 	int track;
-	// Reads in lines from the file as long as they exist
-	while(cin >> song >> time  >> artist >> album >> genre >> track){
-		Song s1(song, track, time) ;
-		timeConvertMtoS(time); // calls time function
+	readFile(argv[1]);
 
-
-	}
 	
 	return 0;
 }
